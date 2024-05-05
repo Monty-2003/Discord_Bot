b99515553dfc7e8c38bfe5d2bd4f5834997af753
Monty-2003 <tmontgomery2021@gmail.com>
2024-05-05 15:47:54 -0400

    Implemented cville crime api, parsing, and db loading

--- db_ingestion.py
+++ db_ingestion.py
@@ -1,20 +1,25 @@
-import pandas as pd
+from api_responses import crime_data
+
 from sqlalchemy import create_engine
+from datetime import datetime
+from pytz import timezone
 
-data = pd.read_csv("./City_Trails.csv")
-
+# connect to db
 engine = create_engine("mysql+mysqlconnector://aba9jj:aba9jj!@database.ds2002.org/aba9jj")
 
 try:
-    data.to_sql(con=engine, name="cville_trails",if_exists="replace", index=False)
-    print("Data successfully uploaded to database aba9jj, table cville_trails")
+    # get data from api
+    crime_df = crime_data.get_crime_data()
+    crime_df.to_sql(con=engine, name="crime",if_exists="replace", index=False)
+    print("Data successfully uploaded to database at EST:", datetime.now(timezone('EST')))
 
 except Exception as e:
     print("An error occured:", e)
-    
+
 # close connection to db
 finally:
     engine.dispose()
+
 
 
 
