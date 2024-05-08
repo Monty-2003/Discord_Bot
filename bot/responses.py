ed8cf1abdfa85a2173d2fd1c7888d1e13208d676
Monty-2003 <tmontgomery2021@gmail.com>
2024-05-08 13:26:11 -0400

    Working on specific commands

--- bot/responses.py
+++ bot/responses.py
@@ -1,17 +1,44 @@
 import os
 import pandas as pd
 import openai
+import pymysql
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, Table, MetaData, select
+from sqlalchemy import create_engine, select, text
+
 
 # Used to produce ChatGPT response if keywords aren't identified
-def ask_openai(question, content=(('', ''), ('', ''))):
+def ask_openai(question, content=None):
     content_for_q = "Your name is AskHoos and you are a helpful assistant specializing in information about Charlottesville, Virginia. You are an adamant University of Virginia Fan! Go Hoos! Users can command !recreation, !transportation, !updates for help with specific Charlottesville issues."
-    content_for_q += content[0][0]
-    content_for_q += content[0][1]
-    content_for_q += content[1][0]
-    content_for_q += content[1][1]
+    
+    # Maximum length allowed for content_for_q
+    max_length = 2000
 
+    # Calculate the remaining space available
+    remaining_space = max_length - len(content_for_q)
+    
+    # If content is not None and there is space available, append it
+    if content is not None and remaining_space > 0:
+        for row in content:
+            row_str = str(row)
+            # Check if adding the row exceeds the remaining space
+            if len(row_str) <= remaining_space:
+                content_for_q += row_str
+                remaining_space -= len(row_str)
+            else:
+                # Truncate the row to fit the remaining space
+                content_for_q += row_str[:remaining_space]
+                break  # Stop appending rows once the limit is reached
+
+    '''
+    # Now content_for_q is guaranteed to be within the 2000 character limit
+    if content is not None:
+        for row in content:
+            content_for_q += str(row)
+    
+    '''
+
+
+    
     response = openai.ChatCompletion.create(
         model="gpt-4-turbo",  # Choose the model you prefer
         messages=[
@@ -35,15 +62,18 @@ def get_response(user_input: str) -> str:
     elif 'bye' in lowered:
         return "Goodbye, friend! Have a great time in Charlottesville. I will be here whenever you need me again!"
     elif 'help' in lowered:
-        return "You can command !recreation, !transportation, or !updates for Charlottesville related information."
+        return "You can command !parks, !trails, !transportation, or !updates for specific Charlottesville related information."
     else:
         load_dotenv()
         OPEN_AI_KEY = os.getenv('OPEN_AI_KEY')
         openai.api_key = OPEN_AI_KEY
 
-        if '!recreation' in lowered:
-            recreation_dat = get_recreation_data()
-            return ask_openai("Give general information and suggestions for ideas based on the provided Charlottesville park and trail system information.", content=recreation_dat)
+        if '!parks' in lowered:
+            recreation_dat = get_park_data()
+            return ask_openai("Give general information and suggestions for ideas based on the given Charlottesville park information.", content=recreation_dat)
+        elif '!trails' in lowered:
+            trail_dat = get_trail_data()
+            return ask_openai("Give general information and suggestions for ideas based on the given Charlottesville trail information.", content=trail_dat)
         elif '!transportation' in lowered:
             return ask_openai(user_input)
         elif '!updates' in lowered:
@@ -52,22 +82,18 @@ def get_response(user_input: str) -> str:
             return ask_openai(user_input)
         
     
-def get_recreation_data():
-    engine = create_engine("mysql+mysqlconnector://aba9jj:aba9jj!@database.ds2002.org/aba9jj")
-    connection = engine.connect()
-    #metadata = MetaData()
-    # get data from db
-    #park_table = Table('parks', metadata)
-    #trail_table = Table('trails', metadata)
 
-    #park_query = select(park_table)
-    #trail_query = select(trail_table)
+
+#### Functions for database querying based on user's commands ####
+def get_park_data():
+    engine = create_engine("mysql+mysqlconnector://aba9jj:aba9jj!@database.ds2002.org/aba9jj")
+    conn = engine.connect()
 
     try:
-       park_result = pd.read_sql("SELECT `PARK_TYPE`, 'PARKNAME' FROM parks WHERE `PARK_TYPE`='COMMUNITY'", engine)
-       trail_result = pd.read_sql('SELECT `NAME`, `TYPE`, `STATUS`, `PROPERTY_OWNER` FROM trails', engine)
+        query = text("SELECT PARKNAME FROM parks WHERE PARK_TYPE = 'COMMUNITY' LIMIT 2")
+        results = conn.execute(query)
 
-       return [("Park information: ", park_result.to_string(index=False)), ("Trail information: ", trail_result.to_string(index=False))]
+        return results 
 
     except Exception as e:
         print("An error occured:", e)
@@ -75,3 +101,22 @@ def get_recreation_data():
     # close connection to db
     finally:
         engine.dispose()
+
+
+def get_trail_data():
+    engine = create_engine("mysql+mysqlconnector://aba9jj:aba9jj!@database.ds2002.org/aba9jj")
+    conn = engine.connect()
+
+    try:
+        query = text("SELECT NAME, TYPE, STATUS FROM trails LIMIT 2")
+        results = conn.execute(query)
+        print(results)
+        return results
+
+    except Exception as e:
+        print("An error occured:", e)
+
+    # close connection to db
+    finally:
+        engine.dispose()
+
